a9420f70e54e906647a9f6d4e43729eb4905c384
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-02-07 09:10:58 -0300

    feat: add project pet_kare, apps: groups, traits and pets and create models

---
+++ pets/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class SexChoise(models.TextChoices):
+    MALE = "Male"
+    FEMALE = "Female"
+    DEFAULT = "Not Informed"
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(
+        max_length=20, choices=SexChoise.choices, default=SexChoise.DEFAULT
+    )
+    group = models.ForeignKey(
+        "groups.Group", on_delete=models.PROTECT, related_name="pets"
+    )
+
+    def __repr__(self) -> str:
+        return f'<[{self.id}] {self.name} - {self.age}>'
---
+++ traits/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+
+class Trait(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    pets = models.ManyToManyField(
+        "pets.Pet",
+        related_name="traits"
+    )
+
+    def __repr__(self) -> str:
+        return f'<[{self.id}] {self.name} - {self.fifa_code}>'
